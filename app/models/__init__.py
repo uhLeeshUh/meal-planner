7eb8cd989a12dad13cc8e10f65d1c025d13dd42e
Alicia Underhill <alicia.underhill@gmail.com>
2025-04-09 14:45:23 -0400

    Set up migrations with alembic

---
+++ app/models/__init__.py
@@ -0,0 +1,11 @@
+from app.models.recipe import Recipe
+from app.models.base_model import BaseModel
+
+# Add new models here as you create them
+# from app.models.new_model import NewModel
+
+__all__ = [
+    'Recipe',
+    'BaseModel',
+    # Add new models to __all__ as you create them
+] 
