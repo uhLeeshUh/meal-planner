f2d943b187c5056efd4aa6c055cdb55f2b883d95
Alicia Underhill <alicia.underhill@gmail.com>
2025-04-10 15:28:05 -0400

    Update BaseModel with timestamps and migrate

--- app/models/base_model.py
+++ app/models/base_model.py
@@ -1,13 +1,20 @@
+from datetime import datetime, timezone
+from sqlalchemy import DateTime
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy.ext.declarative import declared_attr
 from sqlalchemy.dialects.postgresql import UUID
 from app.core.database import Base
 import uuid
 
+def utc_now() -> datetime:
+    return datetime.now(timezone.utc)
+
 class BaseModel(Base):
     __abstract__ = True  # This tells SQLAlchemy this is an abstract base class
     
     id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), default=utc_now, nullable=False)
+    updated_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), default=utc_now, onupdate=utc_now, nullable=False)
     
     # This ensures all subclasses must define __tablename__
     @declared_attr
