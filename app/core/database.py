a369645e542456af272fb66b59980385b7875488
Alicia Underhill <alicia.underhill@gmail.com>
2025-04-10 14:28:30 -0400

    Refactor Base class to SA 2.0 standard

--- app/core/database.py
+++ app/core/database.py
@@ -1,6 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
 import os
 from dotenv import load_dotenv
 
@@ -11,7 +10,8 @@ SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = declarative_base()
+class Base(DeclarativeBase):
+    pass
 
 # Dependency
 def get_db():
